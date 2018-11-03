dd8aff6a7582ac705b3cbe2209560d2daeb7a2a4
Reda Baydoun <reda.baydoun@gmail.com>
2018-11-03 16:01:00 -0400

    [] Play around with go-github lib

--- gotest.go
+++ gotest.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-github/github"
+)
+
+// FetchOrganizations Fetch all the public organizations' membership of a user.
+//
+func FetchOrganizations(username string) ([]*github.Organization, error) {
+	client := github.NewClient(nil)
+	orgs, _, err := client.Organizations.List(context.Background(), username, nil)
+	return orgs, err
+}
 
 func main() {
 	fmt.Println("GoTest")
