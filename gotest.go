dd29d18aed9305ee96e53906fb5c6d6294e54f1b
Reda Baydoun <reda.baydoun@gmail.com>
2018-11-05 22:08:20 -0500

    [] Added listing for PRs on a repo.

--- gotest.go
+++ gotest.go
@@ -16,6 +16,14 @@ func FetchRepositories(username string) ([]*github.Repository, error) {
 	return repos, err
 }
 
+// FetchPullRequests this is a comment
+func FetchPullRequests(owner string, repo string) ([]*github.PullRequest, error) {
+	client := github.NewClient(nil)
+	pulls, _, err := client.PullRequests.List(context.Background(), owner, repo, nil)
+
+	return pulls, err
+}
+
 func main() {
 	var username string
 	fmt.Print("Enter GitHub username: ")
@@ -29,6 +37,15 @@ func main() {
 	}
 
 	for i, repo := range repos {
-		fmt.Printf("%v. %v\n", i+1, repo.GetName())
+		fmt.Printf("%v. %v, owner %v\n", i+1, repo.GetName(), repo.GetOwner().GetLogin())
+		pulls, err := FetchPullRequests(repo.GetOwner().GetLogin(), repo.GetName())
+
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+		}
+
+		for p, pull := range pulls {
+			fmt.Printf("  %v. %v\n", p+1, pull.GetTitle())
+		}
 	}
 }
